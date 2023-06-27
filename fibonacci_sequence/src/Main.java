0692f2ace2168b2cdb687fe0945b802c6b91e33f
Greg Atemi <56564097+greg-atemi@users.noreply.github.com>
2023-06-27 14:28:14 +0300

    Definition and testing of Fibonacci function

---
+++ fibonacci_sequence/src/Main.java
@@ -0,0 +1,36 @@
+//importing Scanner class
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Declaration of body function
+        body();
+    }
+
+    public static void body() {
+        //Definition of body function
+        //Use try-catch block for taking input from the user and handling exception
+        try {
+            Scanner myScanner = new Scanner(System.in);
+            //Variable declaration and initialization
+            int num1 = 0, num2 = 1, num3, x, limit;
+            System.out.println("Input number");
+            limit = Integer.parseInt(myScanner.nextLine());
+            System.out.print(num1+" "+num2);
+            for(x=2; x<limit; x++){
+                num3=num1+num2;
+                if(num3<=limit){
+                    System.out.print(" "+num3);
+                    num1=num2;
+                    num2=num3;
+                }
+            }
+        }
+        //Catching the Number format exception
+        catch (NumberFormatException e) {
+            System.out.println("Invalid input, Please input an integer");
+            //Recalling the function for the user to re-enter the input
+            body();
+        }
+    }
+}
